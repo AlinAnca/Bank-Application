5ca11f5854585a1b7759b650cc5f9a985638599d
Loredana <loredana_surugiu@yahoo.com>
2019-03-24 17:49:27 +0200

    Move InccorectLineExceptions to package exceptions

---
+++ src/main/java/com/bank/application/exceptions/IncorrectLineException.java
@@ -0,0 +1,7 @@
+package com.bank.application.exceptions;
+
+public class IncorrectLineException extends Exception {
+    public IncorrectLineException(String message) {
+        super(message);
+    }
+}
