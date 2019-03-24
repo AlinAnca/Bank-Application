8a2d0c8546772d9ea22ac8c35d765152ef0d1ec4
Loredana <loredana_surugiu@yahoo.com>
2019-03-24 17:55:34 +0200

    Modify main

--- src/main/java/com/bank/application/controller/UserController.java
+++ src/main/java/com/bank/application/controller/UserController.java
@@ -1,38 +1,14 @@
 package com.bank.application.controller;
 
-import com.bank.application.repository.IncorrectLineException;
+import com.bank.application.exceptions.IncorrectLineException;
+import com.bank.application.repository.DataFile;
 import com.bank.application.service.UserLogin;
-
-import java.util.*;
 
 public class UserController {
 
     public static void main(String[] args) throws IncorrectLineException {
-        int option = 0;
-        while(option != 3)
-        {
-            if(option != 1) { option = getOption("1 - Login"); }
-            else {
-                UserLogin obj = new UserLogin();
-                boolean login = obj.run();
-                if(login)
-                {
-                    do {
-                        option = getOption("2 - Logout");
-                        if(option == 2) { System.out.println("Successfully logout.\n"); }
-                    }while(option != 2 && option !=3);
-
-                } else { System.out.println("\nWrong username or password! \nPlease try again.."); }
-            }
-        }
-    }
-
-    private static int getOption(String message)
-    {
-        Scanner keyboard = new Scanner(System.in);
-        System.out.println("Please choose an option:");
-        System.out.println(message);
-        System.out.println("3 - Exit");
-        return keyboard.nextInt();
+        UserLogin login = new UserLogin();
+        login.setUsers(new DataFile().readFile());
+        login.run();
     }
 }
