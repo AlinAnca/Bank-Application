ee3d8a8f352dfb9592d4a8daaae2116c488090a6
Loredana <loredana_surugiu@yahoo.com>
2019-03-24 17:52:53 +0200

    Override equals & hash code

--- src/main/java/com/bank/application/model/User.java
+++ src/main/java/com/bank/application/model/User.java
@@ -1,5 +1,7 @@
 package com.bank.application.model;
 
+import java.util.Objects;
+
 public class User {
     private String username;
     private String password;
@@ -31,5 +33,19 @@ public class User {
                 "username='" + username + '\'' +
                 ", password='" + password + '\'' +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(username, user.username) &&
+                Objects.equals(password, user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(username, password);
     }
 }
